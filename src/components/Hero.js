63f6b909ba33fb68a9c58dd4cf106320afa50357
taniyeahh7 <taniya.ahmed734@gmail.com>
2023-09-30 17:13:49 +0530

    added sliding sidebar

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+
+export default function Hero(){
+    return(
+        <div className="section">
+            <div className="col">
+                <h1 className="titles">What are we about</h1>
+                <p className="info">GourmetFit is youre one-stop destination to practice healthy easting habits and maintaining perfect exercising habits.</p>
+                <button className="btn">explore now</button>
+            </div>
+            <div className="col">
+                col 2
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,54 @@
+
+import {useState} from "react"
+import Sidebar from "./Sidebar.js"
+import {faHome,faList,faCog} from "@fortawesome/free-solid-svg-icons"
+
+export default function Navbar(){
+    const [showSidebar,setShowSideBar]=useState(true);
+    const links=[
+        {
+            name:"Home",
+            path:"/",
+            icon:faHome
+        },
+        {
+            name:"Recipes",
+            path:"/recipes",
+            icon:faList
+        },
+        {
+            name:"Settings",
+            path:"/settings",
+            icon:faCog
+        }
+    ]
+
+    function closeSidebar(){
+        setShowSideBar(false);
+    }
+
+    return(
+        <>
+            <div className="navbar container">
+                <a href="#!" className="logo">F<span>oo</span>diesHub</a>
+                <div className="nav-links">
+                    {/* <a href="#!">Home</a>
+                    <a href="#!">Recipes</a>
+                    <a href="#!">Settings</a> */}
+                    {links.map(link=>(
+                        <a href="#!" key={link.name}>{link.name}</a>
+                    ))}
+                </div>
+                {/* <div onClick={()=>setShowSideBar(!showSidebar)} className='sidebar-btn'> */}
+                <div onClick={() => setShowSideBar(true)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div> 
+                </div>
+            </div>
+            {showSidebar && <Sidebar close={closeSidebar} links={links}/>}
+            {/* <Sidebar links={links}/> */}
+        </>
+        
+    )
+}
