5e3194918053a1440aa0fd93eb93a746b11eafe7
taniyeahh7 <taniya.ahmed734@gmail.com>
2023-09-30 18:32:51 +0530

    improve skills moto added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar.js"
 import Hero from "./components/Hero.js"
+import ImproveSkill from "./components/ImproveSkill.js";
+
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <div className="container main">
-        
         <Hero/>
+        <ImproveSkill/>
       </div>
     </div>
   );
---
+++ src/components/ImproveSkill.js
@@ -0,0 +1,17 @@
+
+
+export default function ImproveSkill(){
+    return (
+        <div className="section improve-skills">
+            <div className="col img">
+                <img src="/img/gallery/img_10.jpg" alt=""/>
+            </div>
+            <div className="col typography">
+                <h1 className="title">Improve Your Cullinary Skills</h1>
+                
+                <button className="btn">Signup now</button>
+            </div>
+            
+        </div>
+    )
+}
