1d80747eef75a5166c32c1c255d10922172b330d
taniyeahh7 <taniya.ahmed734@gmail.com>
2023-12-02 17:41:23 +0530

    settings page

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,10 +1,31 @@
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCheck } from "@fortawesome/free-solid-svg-icons"
+import { useState } from "react"
 
 export default function Settings(){
+    const[theme,setTheme] =useState("light")
     return (
         <div>
-            
-            Settings
+            <div className="section d-block">
+                <h2>Preferred Theme</h2>
+                <div className="options-container">
+                    <div className="option light">
+                        {theme === "light" &&(
+                            <div className="check">
+                            <FontAwesomeIcon icon={faCheck} />
+                        </div>
+                        )}
+                        
+                    </div>
+                    <div className="option dark">
+                        {theme === "dark" &&(
+                            <div className="check">
+                            <FontAwesomeIcon icon={faCheck} />
+                        </div>
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
