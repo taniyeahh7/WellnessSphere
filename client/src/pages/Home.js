a0efd901141ebf72ea538997812584a52880bff4
Anisha Sharma <anisharma968@gmail.com>
2023-12-06 22:40:21 +0530

    added commands to run back-end

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,13 @@
-
-import Hero from "../components/Hero.js"
+import Hero from "../components/Hero.js";
 import ImproveSkill from "../components/ImproveSkill.js";
 import QuoteSection from "../components/QuoteSection.js";
 
-export default function Home(){
-    return (
-        <div>
-            <Hero/>
-            <ImproveSkill/>
-            <QuoteSection/>
-        </div>
-    )
+export default function Home() {
+	return (
+		<div>
+			<Hero />
+			<ImproveSkill />
+			<QuoteSection />
+		</div>
+	);
 }
