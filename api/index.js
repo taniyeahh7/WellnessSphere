a5839eb2a67420d957ff47e3bae36a7246dff12f
Anisha Sharma <anisharma968@gmail.com>
2023-12-08 19:36:24 +0530

    updated back-end

--- api/index.js
+++ api/index.js
@@ -1,40 +1,47 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const dbConnect = require("./config/dbConnect");
-const dotenv = require("dotenv").config();
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import path from "path";
+import userRouter from "./routes/userRoute.js";
+import authRouter from "./routes/authRoute.js";
 
-const authRouter = require("./routes/authRoute");
-const blogRouter = require("./routes/blogRoute");
-const blogCategoryRouter = require("./routes/blogCatRoute");
-const healthConditionsRouter = require("./routes/healthCondRoute");
-const inqRouter = require("./routes/inqRoute");
+dotenv.config();
 
-const morgan = require("morgan");
-const { notFound, errorHandler } = require("./middleware/errorHandler");
+const MONGO_URL = process.env.MONGO_URL;
+mongoose
+	.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log("Connected to MongoDB");
+	})
+	.catch((error) => {
+		console.error("Error connecting to MongoDB:", error);
+	});
+const __dirname = path.resolve();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-dbConnect();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cookieParser());
-app.use(morgan("dev"));
 
-app.use("/api/user", authRouter);
-app.use("/api/blog", blogRouter);
-app.use("/api/blogCategory", blogCategoryRouter);
-app.use("/api/healthConditions", healthConditionsRouter);
-app.use("/api/inquiry", inqRouter);
-
-app.use(notFound);
-app.use(errorHandler);
-
-app.use("/", (req, res) => {
-	res.send("Hello from the server!");
+app.listen(3000, () => {
+	console.log("Server running on port 3000");
 });
 
-app.listen(PORT, () => {
-	console.log(`Server is running at PORT ${PORT}`);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+app.use((err, req, res, next) => {
+	const statusCode = err.statusCode || 500;
+	const message = err.message || "Internal Server Error";
+	return res.status(statusCode).json({
+		success: false,
+		statusCode,
+		message,
+	});
 });
