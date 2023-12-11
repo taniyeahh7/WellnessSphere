699af8bf95617460536e96958e1f8d00836ec55d
Anisha Sharma <anisharma968@gmail.com>
2023-12-11 14:02:07 +0530

    update user model

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -17,6 +17,9 @@ const userSchema = new mongoose.Schema(
 			required: true,
 			unique: true,
 		},
+		healthConditions: {
+			type: Array,
+		},
 		password: {
 			type: String,
 			required: true,
