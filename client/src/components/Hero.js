b57427fec3ee45ca380712e1cad6bbdaf82596db
Anisha Sharma <anisharma968@gmail.com>
2023-12-15 13:40:42 +0530

    revert changes made to home page

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,31 +1,33 @@
-import CustomImage from "./CustomImage.js"
+import CustomImage from "./CustomImage.js";
 
-export default function Hero(){
-    const images=[
-        "/img/gallery/img-curl.png",
-        "/img/gallery/img_2.jpg",
-        "/img/gallery/img_3.jpg",
-        "/img/gallery/img_4.jpg",
-        "/img/gallery/img_exercise.jpg",
-        "/img/gallery/img_6.jpg",
-        "/img/gallery/img_7.jpg",
-        "/img/gallery/img_8.jpg",
-        "/img/gallery/img-pushup.png"
-    ]
+export default function Hero() {
+	const images = [
+		"/img/gallery/img_1.jpg",
+		"/img/gallery/img_2.jpg",
+		"/img/gallery/img_3.jpg",
+		"/img/gallery/img_4.jpg",
+		"/img/gallery/img_5.jpg",
+		"/img/gallery/img_6.jpg",
+		"/img/gallery/img_7.jpg",
+		"/img/gallery/img_8.jpg",
+		"/img/gallery/img_9.jpg",
+	];
 
-    return(
-        <div className="section hero">
-            <div className="col typography">
-                <h1 className="title">What are we about</h1>
-                <p className="info">Wellness Sphere is your one-stop destination to practice healthy eating habits and maintaining perfect exercising habits.</p>
-                <button className="btn">explore now</button>
-            </div>
-            <div className="col gallery">
-                {images.map((src,index)=>(
-                    <CustomImage key={index} imgSrc={src} pt={"90%"}/>
-                ))}
-                
-            </div>
-        </div>
-    )
+	return (
+		<div className="section hero">
+			<div className="col typography">
+				<h1 className="title">What are we about</h1>
+				<p className="info">
+					Wellness Sphere is your one-stop destination to practice healthy
+					eating habits and maintaining perfect exercising habits.
+				</p>
+				<button className="btn">explore now</button>
+			</div>
+			<div className="col gallery">
+				{images.map((src, index) => (
+					<CustomImage key={index} imgSrc={src} pt={"90%"} />
+				))}
+			</div>
+		</div>
+	);
 }
--- client/src/components/ImproveSkill.js
+++ client/src/components/ImproveSkill.js
@@ -1,30 +1,27 @@
+export default function ImproveSkill() {
+	const list = [
+		"Learn new exciting recipes",
+		"Discover deficiency management",
+		"Get customised recipes",
+		"Forget all fitness trainer tantrums",
+		"Exercise with maximising benefits",
+		"Get sole attention of our trainer",
+	];
 
-
-export default function ImproveSkill(){
-    
-    const list=[
-        "Learn new exciting recipes",
-        "Discover deficiency management",
-        "Get customised recipes",
-        "Forget all fitness trainer tantrums",
-        "Exercise with maximising benefits",
-        "Get sole attention of our trainer",
-    ]
-
-
-    return (
-        <div className="section improve-skills">
-            <div className="col img">
-                <img src="/img/gallery/img_home.jpg" alt=""/>
-            </div>
-            <div className="col typography">
-                <h1 className="title">Improve Your Cullinary Skills</h1>
-                {list.map((item,index)=>(
-                    <p className="skill-item" key={index}>{item}</p>
-                ))}
-                <button className="btn">Signup now</button>
-            </div>
-            
-        </div>
-    )
+	return (
+		<div className="section improve-skills">
+			<div className="col img">
+				<img src="/img/gallery/img_10.jpg" alt="" />
+			</div>
+			<div className="col typography">
+				<h1 className="title">Improve Your Cullinary Skills</h1>
+				{list.map((item, index) => (
+					<p className="skill-item" key={index}>
+						{item}
+					</p>
+				))}
+				<button className="btn">Signup now</button>
+			</div>
+		</div>
+	);
 }
