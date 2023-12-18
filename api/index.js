4be5d54f9d621d4c99a16e822e40e86b00bcfac2
Anisha Sharma <anisharma968@gmail.com>
2023-12-18 16:55:17 +0530

    complete login functionality

--- api/index.js
+++ api/index.js
@@ -32,10 +32,10 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/healthCondition", healthConditionRouter);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client/public")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "client", "public", "index.html"));
 });
 
 app.use((err, req, res, next) => {
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,31 +1,78 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
+	const [formData, setFormData] = useState({});
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
+
+	//function to handle changes in the form
+	const handleChange = (e) => {
+		setFormData({
+			...formData,
+			[e.target.id]: e.target.value,
+		});
+	};
+
+	//function to handle submit in the form
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			setLoading(true);
+			const res = await fetch("/api/auth/sign-in", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formData),
+			});
+			const data = await res.json();
+			console.log(data);
+			if (data.success === false) {
+				setLoading(false);
+				setError(data.message);
+				return;
+			}
+			setLoading(false);
+			setError(null);
+			navigate("/choice");
+		} catch (error) {
+			setLoading(false);
+			setError(error.message);
+		}
+	};
+	// ----- front-end code -----
 	return (
 		<div>
 			<div class="center">
 				<h1>Login</h1>
-				<form method="post">
+				<form onSubmit={handleSubmit}>
 					<div class="txt_field">
-						<input type="text" required />
+						<input type="text" required id="username" onChange={handleChange} />
 						<span></span>
 						<label>Username</label>
 					</div>
 					<div class="txt_field">
-						<input type="password" required />
+						<input
+							type="password"
+							required
+							id="password"
+							onChange={handleChange}
+						/>
 						<span></span>
 						<label>Password</label>
 					</div>
-					<div class="pass">Forgot Password?</div>
-					<Link to="/choice">
-						<input type="submit" value="Login" />
-					</Link>
+					<input
+						disabled={loading}
+						type="submit"
+						value={loading ? "Loading..." : "Login"}
+					/>
 					<div class="signup_link">
 						Not a member? <a href="./signup">Signup</a>
 					</div>
 				</form>
 			</div>
-			{/* <h1>hello</h1> */}
 		</div>
 	);
 }
