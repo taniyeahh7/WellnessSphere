0bd8eed366aad94b72022033e0a3ba05de83539e
taniyeahh7 <taniya.ahmed734@gmail.com>
2024-01-17 21:19:46 +0530

    quote section colour fix

--- client/src/components/ImproveSkill.js
+++ client/src/components/ImproveSkill.js
@@ -15,7 +15,7 @@ export default function ImproveSkill() {
 				<img src="/img/gallery/img_10.jpg" alt="" />
 			</div>
 			<div className="col typography">
-				<h1 className="title">Improve Your Cullinary Skills</h1>
+				<h1 className="title">Improve Your Culinary Skills</h1>
 				{list.map((item, index) => (
 					<p className="skill-item" key={index}>
 						{item}
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -48,23 +48,11 @@ export default function Navbar(){
             <div className="navbar container">
                 <Link to="/" className="logo">W<span>ellness</span>Sphere</Link>
                 <div className="nav-links">
-                    {/* <a href="/">Home</a>
-                    <a href="/recipes">Recipes</a>
-                    <a href="/settings">Settings</a>
-                    <a href = "/login">Login</a> */}
                     {links.map(link=>(
                         <Link to={link.path} key={link.name}>{link.name}</Link>
                     ))}
                 </div>
-                {/* <div onClick={()=>setShowSideBar(!showSidebar)} className='sidebar-btn'> */}
-                {/* <div onClick={() => setShowSideBar(true)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
-                    <div className="bar"></div>
-                    <div className="bar"></div>
-                    <div className="bar"></div> this is the original side
-                </div> */}
             </div>
-            {/* {showSidebar && <Sidebar close={closeSidebar} links={links}/>} this also one right under not so sure */}
-            {/* <Sidebar links={links}/> */}
         </>
         
     )
