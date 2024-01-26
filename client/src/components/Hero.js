529b214a5b4b3ea2e967cbb48c9135a5579af3fe
Anisha Sharma <anisharma968@gmail.com>
2024-01-26 09:42:12 +0530

    Update home page with exercise images

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 export default function Hero() {
 	const images = [
-		"/img/gallery/img_1.jpg",
+		"/img/gallery/e_img_1.jpg",
 		"/img/gallery/img_2.jpg",
 		"/img/gallery/img_3.jpg",
-		"/img/gallery/img_4.jpg",
-		"/img/gallery/img_5.jpg",
-		"/img/gallery/img_6.jpg",
+		"/img/gallery/e_img_2.jpg",
+		"/img/gallery/e_img_3.jpg",
 		"/img/gallery/img_7.jpg",
+		"/img/gallery/e_img_4.jpg",
 		"/img/gallery/img_8.jpg",
 		"/img/gallery/img_9.jpg",
 	];
